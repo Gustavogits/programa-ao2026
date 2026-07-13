157ad3b26e7d4aee0434a128b3ac3333d7c849f2
Gustavo <gustavosesi01@gmail.com>
2026-07-13 18:35:12 -0300

    gustavasso

--- gustavinho/quests.js
+++ gustavinho/quests.js
@@ -180,21 +180,202 @@ import leia from "readline-sync"
 
 // QUESTAO 11
 
-var pessoas = 0
-var masc = 0
-var femi = 0
+// var pessoas = 0;
 
-while(pessoas <= 10){
-    var altura = leia.questionFloat("Digite a sua altura: ")
-    var genero = leia.keyInSelect(['MASCULINO','FEMININO'], "Informe o seu genero")
-    if(genero === 0){
-        masc++;
-    } else if(genero === 1){
-        femi++;
-    } else {
-        console.log("Voce nao respondeu!!!")
-    }
-    maiorvalor = (maiorvalor < numero) ? numero : maiorvalor
-    menorvalor = (menorvalor > numero) ? numero : menorvalor
-    valores++;
-}
+// var maiorAltura = 0;
+// var menorAltura = 0;
+
+// var somaAlturas = 0;
+
+// var somaMulheres = 0;
+// var qtdMulheres = 0;
+
+// var qtdHomens = 0;
+
+// while (pessoas < 10) {
+
+//     var genero = leia.keyInSelect(["F", "M"], "INFORME O GENERO");
+
+//     var altura = leia.questionFloat("DIGITE SUA ALTURA: ");
+
+//     if (altura > maiorAltura) {
+//         maiorAltura = altura;
+//     }
+
+//     if (altura < menorAltura) {
+//         menorAltura = altura;
+//     }
+
+//     somaAlturas += altura;
+
+//     if (genero === 0) {
+//         somaMulheres += altura;
+//         qtdMulheres++;
+//     }
+
+//     if (genero === 1) {
+//         qtdHomens++;
+//     }
+
+//     pessoas++;
+// }
+
+// var mediaMulheres = qtdMulheres > 0 ? somaMulheres / qtdMulheres : 0;
+// var mediaPopulacao = somaAlturas / 10;
+// var percentualHomens = (qtdHomens / 10) * 100;
+
+// console.log("\nRESULTADOS");
+// console.log(`Maior altura: ${maiorAltura.toFixed(2)} m`);
+// console.log(`Menor altura: ${menorAltura.toFixed(2)} m`);
+// console.log(`Média das mulheres: ${mediaMulheres.toFixed(2)} m`);
+// console.log(`Média da população: ${mediaPopulacao.toFixed(2)} m`);
+// console.log(`Percentual de homens: ${percentualHomens}%`);
+
+
+
+// QUESTAO 12
+
+// console.log("=== JOGO DO NÚMERO MÁGICO ===");
+// console.log("1 - PvP");
+// console.log("2 - PvE");
+
+// var modo = leia.questionInt("Escolha o modo: ");
+
+// var numeroMagico;
+
+// if (modo === 1) {
+//     console.clear();
+//     numeroMagico = leia.questionInt("Mestre, digite o numero magico: ");
+//     console.clear();
+// } else if (modo === 2) {
+//     console.log("===");
+//     console.log("Escolha a dificuldade:");
+//     console.log("1 - Facil (1 a 10)");
+//     console.log("2 - Medio (1 a 50)");
+//     console.log("3 - Dificil (1 a 100)");
+
+//     var dificuldade = leia.questionInt("Opcao: ");
+//     var max;
+
+//     if (dificuldade === 1) {
+//         max = 10;
+//     } else if (dificuldade === 2) {
+//         max = 50;
+//     } else {
+//         max = 100;
+//     }
+
+//     numeroMagico = Math.floor(Math.random() * max) + 1;
+// } else {
+//     console.log("Modo invalido!");
+//     process.exit();
+// }
+
+// var tentativas = 0;
+// var chute;
+
+// do {
+//     chute = leia.questionInt("Digite seu chute: ");
+//     tentativas++;
+
+//     if (chute > numeroMagico) {
+//         console.log("O numero magico e menor!");
+//     } else if (chute < numeroMagico) {
+//         console.log("O numero magico e maior!");
+//     }
+
+// } while (chute !== numeroMagico);
+
+// console.log("")
+// console.log("Parabens! Voce acertou o numero magico!");
+// console.log("Numero de tentativas: " + tentativas);
+
+
+
+// QUESTAO 13
+
+// var num = leia.questionInt("Digite a quantidade de termos da sequencia Fibonacci: ");
+
+// var a = 1;
+// var b = 1;
+
+// if (num >= 1) {
+//     console.log(a);
+// }
+
+// if (num >= 2) {
+//     console.log(b);
+// }
+
+// for (var i = 3; i <= num; i++) {
+//     var c = a + b;
+//     console.log(c);
+//     a = b;
+//     b = c;
+// }
+
+
+
+// QUESTAO 14
+
+// var numero = leia.questionInt("DIGITE UM NUMERO: ");
+
+// var total = 1;
+
+// while (numero > 1) {
+//     total = total * numero;
+//     numero--;
+// }
+
+// console.log("Fatorial =", total);
+
+
+
+// QUESTAO 15
+
+// var numero = leia.questionInt("Digite um numero decimal: ");
+
+// var binario = "";
+
+// if (numero === 0) {
+//     binario = "0";
+// } else {
+//     while (numero > 0) {
+//         var resto = numero % 2;
+//         binario = resto + binario;
+//         numero = Math.floor(numero / 2);
+//     }
+// }
+
+// console.log("Numero em binario: " + binario);
+
+
+
+// QUESTAO 16
+
+// var maiorMedia = 0;
+// var melhorAluno = "";
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log("")
+//     console.log("Aluno " + i);
+
+//     var nome = leia.question("Nome: ");
+//     var nota1 = leia.questionFloat("Nota 1: ");
+//     var nota2 = leia.questionFloat("Nota 2: ");
+//     var nota3 = leia.questionFloat("Nota 3: ");
+
+//     var media = (nota1 + nota2 + nota3) / 3;
+
+//     console.log("Media: " + media.toFixed(2));
+
+//     if (i === 1 || media > maiorMedia) {
+//         maiorMedia = media;
+//         melhorAluno = nome;
+//     }
+// }
+
+// console.log("")
+// console.log("=== RESULTADO ===");
+// console.log("Aluno com a maior media: " + melhorAluno);
+// console.log("Maior media: " + maiorMedia.toFixed(2));
